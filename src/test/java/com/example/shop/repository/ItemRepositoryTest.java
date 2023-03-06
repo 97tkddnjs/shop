8b164cd2d28180ea2ac79d8ee2efe885a33c574b
Lee Sang Won <tkddnjs206@gmail.com>
2023-03-07 00:42:46 +0900

    r

--- src/test/java/com/example/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/example/shop/repository/ItemRepositoryTest.java
@@ -2,11 +2,16 @@ package com.example.shop.repository;
 
 import com.example.shop.entity.Item;
 import com.example.shop.entity.ItemSellStatus;
+import com.example.shop.entity.QItem;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -19,14 +24,15 @@ class ItemRepositoryTest {
     ItemRepository itemRepository;
 
 
-    @Test
-    @Transactional
-    public void test2(){
+    @PersistenceContext
+    EntityManager em;
+
+    void createItemList() {
         for (int i = 0; i < 10; i++) {
             Item item = new Item();
             item.setItemName("test"+i);
             item.setPrice(1000+i);
-            item.setItemDetail("~~~test des "+i);
+            item.setItemDetail("detail "+i);
             item.setItemSellStatus(ItemSellStatus.SOLD_OUT);
 
             item.setStockNumber(100);
@@ -35,6 +41,13 @@ class ItemRepositoryTest {
             Item saved = itemRepository.save(item);
             System.out.println("saved = " + saved);
         }
+
+    }
+
+    @Test
+    @Transactional
+    public void test2(){
+        this.createItemList();
 
         for (int i = 0;  i < 10; i++) {
             List<Item> itemList = itemRepository.findByItemName("test1");
@@ -61,6 +74,37 @@ class ItemRepositoryTest {
         Item saved = itemRepository.save(item);
         System.out.println("saved = " + saved);
 
+    }
+
+    @Test
+    @Transactional
+    public void test3() {
+
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemDetail("detail");
+        for (Item item : itemList) {
+            System.out.println("item.toString() = " + item.toString());
+        }
+    }
+
+    @Test
+    @Transactional
+    public void queryDSL() {
+        this.createItemList();
+
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+
+        QItem qItem = QItem.item;
+        JPAQuery<Item> query = queryFactory.selectFrom(qItem)
+                .where(qItem.itemSellStatus.eq(ItemSellStatus.SELL))
+                .where(qItem.itemDetail.like("%" + "detail" + "%"))
+                .orderBy(qItem.price.desc());
+
+        List<Item> itemList = query.fetch();
+
+        for (Item item : itemList) {
+            System.out.println("item.toString() = " + item.toString());
+        }
     }
 
 }
