64fff979b5770730df6d21fa6f81d2a789439ed8
97tkddnjs <tkddnjs206@gmail.com>
2023-03-14 19:01:38 +0900

    Shop

--- src/main/java/com/example/shop/service/MemberService.java
+++ src/main/java/com/example/shop/service/MemberService.java
@@ -3,13 +3,17 @@ package com.example.shop.service;
 import com.example.shop.entity.Member;
 import com.example.shop.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class MemberService {
+public class MemberService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
 
@@ -23,6 +27,19 @@ public class MemberService {
         if (findMember != null) {
             throw new IllegalStateException("already member");
         }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member member = memberRepository.findByEmail(email);
+        if (member == null) {
+            throw new UsernameNotFoundException(email);
+        }
+        return User.builder()
+                .username(member.getEmail())
+                .password(member.getPassword())
+                .roles(member.getRole().toString())
+                .build();
     }
 }
 
