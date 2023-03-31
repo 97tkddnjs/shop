9dace65e97a6da6b3a5a6df6f29f9e4b5af603db
97tkddnjs <tkddnjs206@gmail.com>
2023-03-31 19:57:46 +0900

    update

--- src/main/java/com/example/shop/entity/Order.java
+++ src/main/java/com/example/shop/entity/Order.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Getter
 @Setter
 @Table(name="orders")
-public class Order {
+public class Order extends BaseEntity{
 
     @Id
     @GeneratedValue
@@ -34,8 +34,6 @@ public class Order {
             orphanRemoval = true , fetch = FetchType.LAZY)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    private LocalDateTime regTime;
 
-    private LocalDateTime updateTime;
 
 }
