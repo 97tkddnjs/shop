f7144bb37a9c87dab19d02d674252cfb08e5d69c
97tkddnjs <tkddnjs206@gmail.com>
2023-04-07 13:06:32 +0900

    update

--- src/main/java/com/example/shop/controller/ItemController.java
+++ src/main/java/com/example/shop/controller/ItemController.java
@@ -1,13 +1,17 @@
 package com.example.shop.controller;
 
+import com.example.shop.dto.ItemFormDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ItemController {
 
     @GetMapping("/admin/item/new")
-    public String inform() {
+    public String inform(Model model) {
+        model.addAttribute("itemFormDto", new ItemFormDto());
         return "/item/itemForm";
     }
+
 }
